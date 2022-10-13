573dadb2cafcdf3e40653a8c87d4e2273d2183f6
Hanane <hananeouj@gmail.com>
2022-10-13 13:25:13 -0700

    Task-8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry level into the program
+ *
+ * Return: 1 if successful. Other numbers if not
+ */
+int main(void)
+{
+	system("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
