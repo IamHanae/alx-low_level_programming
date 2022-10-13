7074563ba4b9297dd17ef83b9f59fc67e481431e
Hanane <hananeouj@gmail.com>
2022-10-13 11:18:17 -0700

    Task-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - Entry point into the program
  * 
  * Return: 0 if successful, other numbers otherwise
- */
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
