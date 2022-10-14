dc9a856bee526d7297344682513e83d4739f4115
Hanane <hananeouj@gmail.com>
2022-10-14 11:31:27 -0700

    Task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Return: 0 on success. Error otherwise
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf(i);
+	return (0);
+}
