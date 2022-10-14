148432351d9889bc6159dd208849431e43ef0650
Hanane <hananeouj@gmail.com>
2022-10-14 10:33:53 -0700

    Task-2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Return: 0 if success. error otherwise
+ */
+int main(void)
+{
+	int i;
+	char s[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i <= 26; i++)
+		putchar("%c", s[i]);
+	return (0);
+}
