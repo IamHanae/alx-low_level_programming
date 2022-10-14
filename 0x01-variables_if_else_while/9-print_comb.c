0368a6729980cd0b829347e9b250c60d6977d25e
Hanane <hananeouj@gmail.com>
2022-10-14 13:45:14 -0700

    Task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Return: 0 on success. Error otherwise
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}
