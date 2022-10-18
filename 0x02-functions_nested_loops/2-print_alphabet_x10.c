5b21d8dd9d4b0e21454b7e8efb4f97d0f826fbb2
Hanane <hananeouj@gmail.com>
2022-10-18 15:42:01 -0700

    Task-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet
+ *
+ * Return: Void
+ */
+void print_alphabet_10x(void)
+{
+	int i;
+	int count = 0;
+	char s[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (count < 10)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+		count++;
+	}
+	
+}
