783181ecc10628c5663dabebce67c2d1167096fd
Hanane <hananeouj@gmail.com>
2022-10-19 07:15:24 -0700

    Task-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @n: The first number to add
+ * @m: The second number to add
+ *
+ * Return: The result of the addition
+ */
+int add(int n, int m)
+{
+	int res;
+	
+	res = n + m;
+
+	return (res);
+}
