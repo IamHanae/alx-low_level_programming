9ff6a1a68b1946560a15d4f0c4ac0d333868a1e9
Hanane <hananeouj@gmail.com>
2022-10-19 15:57:00 -0700

    Task-14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int n1 = 0;
+	int n2 = 1;
+	int n3;
+
+	for (i = 0; i < 50; i++)
+	{
+		n3 = n1 + n2;
+		printf("%i, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	return (0);
+}
