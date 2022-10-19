8e52165a7e40ebb6a70bf297b2ac6279cc5ac6bc
Hanane <hananeouj@gmail.com>
2022-10-19 16:49:13 -0700

    Task-14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,16 +8,21 @@
 int main(void)
 {
 	int i;
-	int n1 = 1;
-	int n2 = 2;
+	int n1 = 0;
+	int n2 = 1;
 	int n3;
 
 	for (i = 0; i < 50; i++)
 	{
+		if (i <= 1)
+			n3 = i;
+		else
+		{
 		n3 = n1 + n2;
-		printf("%i, ", n3);
 		n1 = n2;
 		n2 = n3;
+		}
+		printf("%d\n", n3);
 	}
 	return (0);
 }
