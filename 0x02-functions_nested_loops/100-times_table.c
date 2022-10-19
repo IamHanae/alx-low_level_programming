7fe4839ea702f8700f3174142484e5d4adbe28e4
Hanane <hananeouj@gmail.com>
2022-10-19 09:17:21 -0700

    Task-12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,6 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 		return;
-	_putchar('0');
-	_putchar(',');
-	_putchar(' ');
 
 	for (i = 0; i <= n; i++)
 	{
@@ -23,7 +20,7 @@ void print_times_table(int n)
 		{
 			printf("%i ,", i * j);
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 
 }
