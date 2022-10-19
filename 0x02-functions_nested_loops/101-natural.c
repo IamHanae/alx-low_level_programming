92fe57987ec360ebe252b4df544fb9bd0177aa6e
Hanane <hananeouj@gmail.com>
2022-10-19 15:47:14 -0700

    Task-13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level into the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 1024;
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%i\n", sum);
+	Return (0);
+}
