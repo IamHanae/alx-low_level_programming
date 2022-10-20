bdfaca51fff92246ab47fa36ec62581ae31de38e
Hanane <hananeouj@gmail.com>
2022-10-20 05:10:02 -0700

    Task-16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,6 @@ int main(void)
 	int i;
 	long int n1 = 1, n2 = 2, m1, m2, n11, n22;
 	int bool1 = 1;
-	int bool2 = 1;
 
 	printf("%ld, %ld", n1, n2);
 	for (i = 0; i < 96; i++)
@@ -24,14 +23,11 @@ int main(void)
 		}
 		else
 		{
-			if (bool2)
-			{
-				n11 = n1 % 1000000000;
-				n22 = n2 % 1000000000;
-				n1 = n1 / 1000000000;
-				n2 = n2 / 1000000000;
-				bool2 = 0;
-			}
+			n11 = n1 % 1000000000;
+			n22 = n2 % 1000000000;
+			n1 = n1 / 1000000000;
+			n2 = n2 / 1000000000;
+
 			m2 = (n11 + n22);
 			m1 = n1 + n2 + (m2 / 1000000000);
 			printf(", %ld", m1);
@@ -41,7 +37,7 @@ int main(void)
 			n2 = m1;
 			n22 = (m2 % 1000000000);
 		}
-		if (((n1 + n2) < 0) && bool1 == 1)
+		if ((n1 + n2) < 0)
 			bool1 = 0;
 	}
 	printf("\n");
