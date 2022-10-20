09ca33540d2482e267b9f7b4bfd8ffddf7dedeef
Hanane <hananeouj@gmail.com>
2022-10-20 13:45:02 -0700

    Task-5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
-			_putchar(j + '0');
-
+		for (j = '0'; j < '15'; j++)
+		{
+			if (j < 10)
+				_putchar(j + '0');
+		}
 		_putchar('\n');
 	}
 }
