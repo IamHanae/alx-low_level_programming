43b9d7899e49c8680823c7fb32825f8ad7489491
Hanane <hananeouj@gmail.com>
2022-10-19 18:33:27 -0700

    Task-15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Result: Always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long n1, n2, n3;
+	float total;
+
+	while (1)
+	{
+		n3 = n1 + n2;
+		if (n3 > 4000000)
+			break;
+		if((n3 % 2) == 0)
+			total += n3;
+		n1 = n2;
+		n2 = n3;
+	}
+	print("%.0f\n", total);
+
+	return (0);
+}
