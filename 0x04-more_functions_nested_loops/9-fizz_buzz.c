bb315e9d92f15ac800278698892a8937e2330901
Hanane <hananeouj@gmail.com>
2022-10-20 18:34:29 -0700

    Task-9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", i);
+		if (i != 100)
+			_putchar(' ');
+	}
+	_putchar('\n');
+	return (0);
+}
