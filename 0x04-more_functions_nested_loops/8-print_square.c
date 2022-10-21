df48678474c6fd190e90cca55d4e0a1bf501f49e
Hanane <hananeouj@gmail.com>
2022-10-20 18:06:08 -0700

    Task-8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * followed by a new line
+ * Return: Void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
