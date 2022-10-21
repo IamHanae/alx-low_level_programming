66bb3a2a34d4118310ecfa93f9e0c6c0bfdff861
Hanane <hananeouj@gmail.com>
2022-10-20 19:20:14 -0700

    Task-10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,5 +5,25 @@
  * @size: Size of the triangle
  * Return: Void
  */
-void print_triangle(int size
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			if(i != size - 1)
+				_putchar('\n');
+		}
+	}
+	_putchar('\n');
 }
