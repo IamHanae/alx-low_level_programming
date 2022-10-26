3b8e6ff31d816afcfe075dc014cebd83663fdd89
Hanane <hananeouj@gmail.com>
2022-10-25 17:52:20 -0700

    Task-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse
+ * @s: The string to reverse
+ * Return: Void
+ */
+void print_rev(char *s)
+{
+	int i;
+	char srev[];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		i++;
+	}
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
