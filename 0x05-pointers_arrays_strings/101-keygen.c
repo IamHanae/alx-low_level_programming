b5ac39828f4a3f8a8a7de518c953e660fb26f9a5
Hanane <hananeouj@gmail.com>
2022-10-25 20:36:53 -0700

    Task-11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generatee random valid password
+ * Return: Always 0
+ */
+int main(void)
+{
+	int random = 0;
+	int c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		random = rand() % 128;
+		if (c < 2772)
+			break;
+		c += r;
+		printf("%c", random);
+	}
+
+	printf("%c\n", (2772 - c));
+
+	Return (0);
+}
