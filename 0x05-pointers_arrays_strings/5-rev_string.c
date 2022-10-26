12e9bfc04609735b105b0e79a5d1532cfcdc79be
Hanane <hananeouj@gmail.com>
2022-10-25 18:33:33 -0700

    Task-5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: String to reverse
+ * Return: Void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+}
