3ea9fa2a2295b5eca3fb771530c4b291a9cbb8d9
Hanane <hananeouj@gmail.com>
2022-10-25 17:22:47 -0700

    Task-2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Mesure the length of a string
+ * @s: The string to mesure
+ * Return: The length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i + 1);
+}
