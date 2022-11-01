ff390226ea3337b10450d293c6d1342ac1c53ceb
Hanane <hananeouj@gmail.com>
2022-11-01 04:06:54 -0700

    Task-5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,7 +25,7 @@ char *_strstr(char *haystack, char *needle)
 				if (haystack[i + 1] == '\0')
 					return (haystack);
 				i++;
-			} while (haystack[i] == needle [i]);
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
