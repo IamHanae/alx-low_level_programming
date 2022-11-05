6977cb99f3c90b4482501696bec99430e6eafc37
Hanane <hananeouj@gmail.com>
2022-11-05 07:57:25 -0700

    Task-8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -35,3 +35,44 @@ void iter_wild(char **s)
 }
 
 /**
+ * postfix_match - Check if two strings match.
+ * @s: The string to match.
+ * @postfix: The postfix
+ * Return: If identical, a pointer to the last byte location
+ * Otherwise, a pointer to the first unmatch char
+ */
+char *postfix_match(char *s, char *postfix)
+{
+	int len = strlen_noWilds(s) - 1;
+	int postfix_len = strlen_noWilds(postfix) - 1;
+
+	if (*postfix == '*')
+		iter_wild(&postfix);
+	if (*(s + len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (postfix_match(s, postfix));
+	}
+	return (postfix);
+}
+
+/**
+ * wildcmp - Compare two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * Return: If identical 1, Otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		iter_wild(&s2);
+		s2 = postfix_match(s1, s2);
+	}
+
+	if (*s2 == '\0')
+		return (1);
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(++s1, ++s2));
+}
