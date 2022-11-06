0d5a0fb7a26389795bae5c9c9fb964928b0bc10a
Hanane <hananeouj@gmail.com>
2022-11-06 08:11:38 -0800

    Task-0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level into the program
+ * @argc: The number of arguments supplied.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
+ */
+int main(int argc, chat *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
