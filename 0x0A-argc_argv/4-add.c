0036840fcd952f0926de6ab39fb16e1b5073d8fb
Hanane <hananeouj@gmail.com>
2022-11-08 04:21:45 -0800

    Task-4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the addition of positive numbers.
+ * @argc: The number of arguments.
+ * @argv: An array of pointers to the arguments.
+ * Return: If one number cotains symbols - 1. Oherwise - 0.
+ */
+int main (int argc, char *argv[])
+{
+	int num1, num2, sum = 0;
+
+	for (num1 = 1; num1 < argc; num1++)
+	{
+		for (num2 = 0; argv[num1][num2]; num2++)
+		{
+			if (argv[num1][num2] < '0' || argv[num1][num2] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[num1]);
+	}
+	printf("%i\n", sum);
+	return (0);
+}
