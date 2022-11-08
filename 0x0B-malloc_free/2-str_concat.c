db8eb94f517040e6570275d619e13247d3f7fb49
Hanane <hananeouj@gmail.com>
2022-11-08 15:49:02 -0800

    Task-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,22 +10,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
+	char *concat_str;
 	int i, j = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s[i] || s2[i]; i++)
+	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
-	concat = malloc(sizeof(char) * len);
+	concat_str = malloc(sizeof(char) * len);
 
-	if (concat == NULL)
+	if (concat_str == NULL)
 		return (NULL);
-	for (i = 0; s[i]; i++)
-		concat[j++] = s1[i];
-	for (i = 0; s[i]; i++)
-		concat[j++] = s2[i];
-	return (concat);
+	for (i = 0; s1[i]; i++)
+		concat_str[j++] = s1[i];
+	for (i = 0; s2[i]; i++)
+		concat_str[j++] = s2[i];
+	return (concat_str);
 }
